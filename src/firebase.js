94ea15239c64a6003599012656920a8c5cef121f
TONI <eragatony@gmail.com>
2020-09-03 03:39:29 +0100

    added sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBBgJytA30tPKi1S7WpFx2Dg4rGHu7ngJo",
+	authDomain: "whats-app-5b6cc.firebaseapp.com",
+	databaseURL: "https://whats-app-5b6cc.firebaseio.com",
+	projectId: "whats-app-5b6cc",
+	storageBucket: "whats-app-5b6cc.appspot.com",
+	messagingSenderId: "970903979248",
+	appId: "1:970903979248:web:68e20f8f89a5fb728d6e35",
+	measurementId: "G-BY8YMY579B",
+};
